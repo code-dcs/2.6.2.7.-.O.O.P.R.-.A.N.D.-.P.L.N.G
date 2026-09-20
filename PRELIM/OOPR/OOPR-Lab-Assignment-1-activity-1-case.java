1c7c48b8f732bdcda9dfad1c4a812c628640dabe
code-dcs <dcn.sc.starlight@gmail.com>
2026-09-20 09:58:26 +0900

    JAVA Update

---
+++ PRELIM/OOPR/OOPR-Lab-Assignment-1-activity-1-case.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    int check = 1;
+    Scanner scan = new Scanner(System.in);
+    while(check == 1) {
+      double jscore = 0, sscore = 0, dscore = 0;
+      String ext = "";
+      String grade= "";
+
+      System.out.println("Input:\nJava Score:");
+      jscore = scan.nextDouble();
+      System.out.println("C Score:");
+      sscore = scan.nextDouble();
+      System.out.println("Database Handling Score:");
+      dscore = scan.nextDouble();
+
+      double score = (jscore + sscore + dscore) / 3.0;
+      int choiceScore = (int) (score/5);
+
+      switch (choiceScore) {
+        case 20, 19, 18:
+          grade = "A";
+          break;
+        case 17, 16:
+          grade = "B";
+          break;
+        case 15:
+          grade = "C";
+          break;
+        default:
+          grade = "F";
+      }
+
+      if(score >= 0 && score <= 100) {
+        System.out.println("Grade: " + grade);
+        System.out.println("Explanation: The average of the student is " + String.format("%.2f", score) + " so the student's grade is " + grade);
+      }
+
+      System.out.println("Do you want to continue: (YES / NO)");
+			ext = scan.next();
+			if(ext.equalsIgnoreCase("NO")) {
+        System.out.println("Program terminated.\nThank you!");
+				check--;
+			}
+      System.out.println("\n");
+    }
+  }
+}
